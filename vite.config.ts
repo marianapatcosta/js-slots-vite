29c80d464544c393ec1efe055e24bf2abcd17868
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2022-06-05 18:19:23 +0100

    implement pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 const path = require('path');
 
 export default defineConfig({
@@ -8,7 +9,63 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      base: '/',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      strategies: 'injectManifest',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'JS Slots',
+        short_name: 'JS Slots',
+        theme_color: '#06080f',
+        description:
+          'JS Slot Machine is a regular slot machine game that has the logos of main JS-related tools as symbols.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#06080f',
+        icons: [
+          {
+            src: 'images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'images/apple-touch-icon.png',
+            type: 'image/png',
+          },
+          {
+            src: 'images/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png',
+          },
+          {
+            src: 'images/safari-pinned-tab.png',
+            type: 'image/png',
+          },
+        ],
+      },
+      injectManifest: {
+        globPatterns: ['**.{html, js, css, svg, json}', '**'],
+        globIgnores: ['**/node_modules/**/*'],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+        globIgnores: ['**/node_modules/**/*'],
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
