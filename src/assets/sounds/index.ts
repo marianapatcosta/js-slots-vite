768b1de50bc127b4216eebf9f63daec2439404b9
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2022-06-07 01:38:17 +0100

    add sound types and loseSound

--- src/assets/sounds/index.ts
+++ src/assets/sounds/index.ts
@@ -1,6 +1,7 @@
 import BubblePressSound from './mixkit-plastic-bubble-click-1124.wav';
 import CasinoPressSound from './mixkit-retro-arcade-casino-notification-211.wav';
 import CoinsSound from './mixkit-coins-handling.wav';
+import LoseSound from './mixkit-retro-arcade-lose-2027.wav';
 import SlotWheelSound from './mixkit-slot-machine-wheel-1932.wav';
 import SelectPressSound from './mixkit-select-click-1109.wav';
 import ThemeSound from './music-box-monster.mp3';
@@ -10,6 +11,7 @@ export {
   BubblePressSound,
   CasinoPressSound,
   CoinsSound,
+  LoseSound,
   SelectPressSound,
   SlotWheelSound,
   ThemeSound,
